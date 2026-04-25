8a6c73c375448a30a6f83e0cdc046613566a3af5
infinito2100-lab <infinito2100@gmail.com>
2026-04-25 16:13:18 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const admin = require("firebase-admin");
+const db = admin.firestore();
 
 const app = express();
 
